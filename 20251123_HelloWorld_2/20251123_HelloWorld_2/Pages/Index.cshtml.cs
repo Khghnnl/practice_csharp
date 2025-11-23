d237fc17f08e0cb821f99437b40a98ce6c911065
hrtmtkhs <m09c22@gmail.com>
2025-11-23 20:47:42 +0900

    Add 20251123_HelloWorld_2

---
+++ 20251123_HelloWorld_2/20251123_HelloWorld_2/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _20251123_HelloWorld_2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
